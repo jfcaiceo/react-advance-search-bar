c0bf871cc714d6e95419a3f6bf39f7bddf75cdeb
José Lezama <joseglego@gmail.com>
2019-03-08 13:25:57 -0300

    feat(listHelper): add support for arrows/scape buttons

--- src/lib/components/InputOptionListHelper.jsx
+++ src/lib/components/InputOptionListHelper.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import PropTypes from 'prop-types';
 import InputOptionList from './InputOptionList.jsx';
 import './InputOptionListHelper.css';
@@ -7,6 +8,12 @@ export default class InputOptionListHelper extends React.Component {
   constructor (props) {
     super(props);
     this.handleOptionSelect = this.handleOptionSelect.bind(this);
+    this.onKeyPress = this.onKeyPress.bind(this);
+    this.onArrowUp = this.onArrowUp.bind(this);
+    this.onArrowDown = this.onArrowDown.bind(this);
+    this.onEnter = this.onEnter.bind(this);
+    this.onBackspace = this.onBackspace.bind(this);
+    this.redraw = this.redraw.bind(this);
   }
 
   handleOptionSelect (optionSelect) {
@@ -14,12 +21,88 @@ export default class InputOptionListHelper extends React.Component {
     this.props.changeHelperDisplay(false);
   }
 
+  onKeyPress (event) {
+    switch (event.key) {
+      case 'ArrowUp':
+        this.onArrowUp();
+        break;
+      case 'ArrowDown':
+        this.onArrowDown();
+        break;
+      case 'Enter':
+        this.onEnter(event);
+        break;
+      case 'Tab':
+        this.onEnter(event);
+        break;
+      case 'Backspace':
+        this.onBackspace();
+        break;
+      case 'Escape':
+        this.props.changeHelperDisplay(false);
+        break;
+    }
+  }
+
+  redraw (value) {
+    const modal = document.getElementsByClassName('search-bar__input-options-list-helper-modal')[0];
+    const list = document.getElementsByClassName('search-bar__input-options-list ')[0];
+    const selected = list.children[0].children[value];
+
+    if (!modal || !list || !selected) return;
+
+    if (selected.offsetTop >= modal.offsetTop + modal.offsetHeight) {
+      modal.scroll({ top: selected.offsetTop - 10, behavior: 'smooth' });
+    } else if (selected.getBoundingClientRect().y <= modal.getBoundingClientRect().y) {
+      modal.scroll({ top: selected.getBoundingClientRect().y - 10, behavior: 'smooth' });
+    }
+  }
+
+  onArrowUp () {
+    const value = Math.max(0, this.props.selectedOption - 1);
+    this.redraw(value);
+    this.props.changeSearchIndexSelected(value);
+  }
+
+  onArrowDown () {
+    const children = React.Children.toArray(this.props.children);
+    const value = Math.min(children.length - 1, this.props.selectedOption + 1);
+    this.redraw(value);
+    this.props.changeSearchIndexSelected(value);
+  }
+
+  onEnter (event) {
+    event.preventDefault();
+    let options = getFilteredChildren(React.Children.toArray(this.props.children), this.props.value);
+
+    if (options.length === 0) {
+      this.props.changeHelperDisplay(true);
+    } else {
+      this.props.onOptionSelect(options[this.props.selectedOption]);
+    }
+  }
+
+  onBackspace () {
+    if (this.props.value.length === 0) {
+      this.props.handleOptionDelete();
+    }
+  }
+
+  componentDidMount () {
+    this.ref.focus();
+    window.addEventListener('keydown', this.onKeyPress);
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('keydown', this.onKeyPress);
+  }
+
   render () {
     return (
       <div className='search-bar__input-options-list-helper'>
         <div className='search-bar__input-options-list-helper-backdrop'
           onClick={() => this.props.changeHelperDisplay(false)} />
-        <div className='search-bar__input-options-list-helper-modal'>
+        <div className='search-bar__input-options-list-helper-modal' tabIndex='0' ref={ref => { this.ref = ref; }}>
           <h3>{this.props.helperTitle}</h3>
           <InputOptionList onOptionSelect={this.handleOptionSelect}
             currentSearchingKey=''
@@ -36,6 +119,7 @@ export default class InputOptionListHelper extends React.Component {
 }
 
 InputOptionListHelper.propTypes = {
+  handleOptionDelete: PropTypes.func.isRequired,
   handleOptionSelect: PropTypes.func.isRequired,
   changeHelperDisplay: PropTypes.func.isRequired,
   changeSearchIndexSelected: PropTypes.func,
