13a655d8aa2ec21ab142433bb274c51d8884ce50
José Lezama <joseglego@gmail.com>
2019-03-18 16:55:12 -0300

    feat(input-chip): delete with click any chip in the input

--- src/lib/components/Input.jsx
+++ src/lib/components/Input.jsx
@@ -10,6 +10,7 @@ export default class Input extends React.Component {
     this.triggerInputStart = this.triggerInputStart.bind(this);
     this.onChange = this.onChange.bind(this);
     this.onKeyPress = this.onKeyPress.bind(this);
+    this.onDelete = this.onDelete.bind(this);
   }
 
   componentDidMount () {
@@ -26,6 +27,11 @@ export default class Input extends React.Component {
 
   onChange (event) {
     this.props.onInputChange(event.target.value, this.props.inputOption);
+  }
+
+  onDelete () {
+    this.props.onInputChange('', this.props.inputOption);
+    this.props.triggerInputEnd();
   }
 
   onKeyPress (event) {
@@ -53,15 +59,20 @@ export default class Input extends React.Component {
     const size = Math.max(this.props.value.length, 1);
     return (
       <div className='search-bar__input-tag'>
-        <span>
+        <span className='input-tag__start'>
           { getInputDisplayName(this.props.inputOption) }
         </span>
-        <input type='text'
-          value={this.props.value}
-          size={size}
-          onChange={this.onChange}
-          onKeyDown={this.onKeyPress}
-          ref={this.setTextInputRef.bind(this)} />
+        <span className='input-tag__second'>
+          <input type='text'
+            value={this.props.value}
+            size={size}
+            onChange={this.onChange}
+            onKeyDown={this.onKeyPress}
+            ref={this.setTextInputRef.bind(this)} />
+          <span className='input-tag__delete' onClick={this.onDelete}>
+            <svg className='input-tag-delete__icon' xmlns='http://www.w3.org/2000/svg' width='14' height='14' viewBox='0 0 24 24'><path fill='none' d='M0 0h24v24H0V0z' /><path d='M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z' /></svg>
+          </span>
+        </span>
       </div>
     );
   }
